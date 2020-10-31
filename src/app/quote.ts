8fda4538b5e08547ebe80979d07294b2ec1367e1
Belyse Uwambayinema <ubelyse1@gmail.com>
2020-10-31 10:12:36 +0200

    added the creation datetime

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,8 +1,9 @@
+import { Time } from '@angular/common';
+
 export class Quote {
     showAuthor:boolean;
     
-    constructor(public id:number, public  name: string, public  author:string){
+    constructor(public id:number, public  name: string, public  author:string, public createdDate:Date){
         this.showAuthor=false;
     }
- 
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,9 +11,9 @@ export class QuoteComponent implements OnInit {
   quotes:Quote[]=
   [
     
-      new Quote(1,'Get busy living or get busy dying', 'Stephen King'),
-      new Quote(2,'The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself','Mark Caine'),
-      new Quote(3, 'When one door of happiness closes, another opens; but often we look so long at the closed door that we do not see the one which has been opened for us', 'Helen Keller'),
+      new Quote(1,'Get busy living or get busy dying', 'Stephen King',new Date(2020,11,1)),
+      new Quote(2,'The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself','Mark Caine',new Date(2020,11,5)),
+      new Quote(3, 'When one door of happiness closes, another opens; but often we look so long at the closed door that we do not see the one which has been opened for us', 'Helen Keller',new Date(2020,11,10)),
   
   ];
 
