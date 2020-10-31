0211c8482dcaeaab03874d8559073df6c8d21d9f
Belyse Uwambayinema <ubelyse1@gmail.com>
2020-10-31 09:52:20 +0200

    added the highlight directive

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -3,9 +3,18 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 @Directive({
   selector: '[appHighlight]'
 })
-
 export class HighlightDirective {
-  constructor(elem:ElementRef) {
-    elem.nativeElement.style.backgroundColor="darkred"
-   }
+  constructor(private el: ElementRef) { }
+
+  @HostListener('click') onClicks() {
+    this.highlight('yellow');
+  }
+
+  @HostListener('dblclick') onDoubleClicks() {
+    this.highlight(null);
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor= color;
+  }
 }
