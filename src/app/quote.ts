8efc072a7ab85c6e2ab7738771450bb28126f727
Belyse Uwambayinema <ubelyse1@gmail.com>
2020-11-01 08:22:07 +0200

    added the validation to the form

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -3,7 +3,7 @@ import { Time } from '@angular/common';
 export class Quote {
     showAuthor:boolean;
     
-    constructor(public id:number, public  name: string, public  author:string, public createdDate:Date){
+    constructor(public id:number, public  name: string, public  author:string,public submitedby: string, public createdDate:Date){
         this.showAuthor=false;
     }
 }
