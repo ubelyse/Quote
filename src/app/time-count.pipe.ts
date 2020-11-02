057ded31577ca4882dcc7723785d7e00cf6857ee
Belyse Uwambayinema <ubelyse1@gmail.com>
2020-11-02 08:38:08 +0200

    added the timecount pipe

--- src/app/time-count.pipe.ts
+++ src/app/time-count.pipe.ts
@@ -6,19 +6,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TimeCountPipe implements PipeTransform {
 
   transform(value: any): number {
-    let today:Date = new Date (); //get current date and time
-    let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(), today.getDate())
-    var dateDifference= todayWithNoTime-value //returns value in milliseconds
-    const secondsInADay=86400; //60 sec*60 min in an hr * 24 hrs
+    let today: Date= new Date();
+    let hours = Math.abs((today.getTime() - value.getTime()) / 3600000) 
 
-    var dateDifferenceSeconds=dateDifference*0.001; //converts to seconds
+    return hours;
+    
+    
+    /*let today: Date = new Date();
+    let todayWithNoTime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    var dateDifference = Math.round(Math.abs(value - todayWithNoTime));
+    const secondsInDay = 86400;
+    var dateDifferenceSeconds = dateDifference * 0.001;
+    var dateCounter = Math.round(dateDifferenceSeconds / secondsInDay);
 
-    var TimeCounter = dateDifferenceSeconds/secondsInADay;
-
-    if (TimeCounter >=1){
-      return Math.round(TimeCounter);
-    }else{
+    if (dateCounter >= 1 && value < todayWithNoTime) {
+      return dateCounter;
+    } else {
       return 0;
-    }
+    }*/
   }
 }
